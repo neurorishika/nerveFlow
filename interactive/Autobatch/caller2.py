6b121a34f2f696fc6134c91557efb188f7fd9a3e
Saptarshi <saptarshi.mohanta@gmail.com>
2019-01-24 17:49:01 +0530

    Plis work

---
+++ interactive/Autobatch/caller2.py
@@ -0,0 +1,22 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+from subprocess import call
+import numpy as np
+
+total_time = int(input("Enter Simulation Length ( Default: 2000 ) : ") or "2000")
+n_splits = int(input("Enter Number of Splits ( Default: 2 ) : ") or "2")
+time = np.split(np.arange(0,total_time,0.01),n_splits)
+
+for n,i in enumerate(time):
+    if n>0:
+        time[n] = np.append(i[0]-0.01,i)
+
+np.save("time",time)
+
+for i in range(n_splits):
+    call(['python','run2.py',str(i)])
+
